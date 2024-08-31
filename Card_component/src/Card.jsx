fc65872d6ba9e727a44414e6a937591bb0167e74
RoshanPanthi11 <roshanpanthi13@gmail.com>
2024-08-31 15:57:33 +0545

    updated

---
+++ Card_component/src/Card.jsx
@@ -0,0 +1,14 @@
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img  className="card_img" src="https://via.placeholder.com/150" alt="profile Picture"></img>
+            <h2 className="card_tittle">Panthi</h2>
+            <p className="card_dis">I am a learner and i try to learn new thing day by day</p>
+
+        </div>
+    )
+}
+
+export default Card
